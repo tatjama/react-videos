43e9bddfea827f33455dd251852947d0f98d30be
tatjama <tanja120a@gmail.com>
2020-02-29 11:13:14 +0100

    Updates

---
+++ precache-manifest.bd0b81f8ccc60b9ad92be1f249cb557c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a3184011c54a23205ee4777f593102",
+    "url": "/react-videos/index.html"
+  },
+  {
+    "revision": "30589aab593373e89433",
+    "url": "/react-videos/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a41d870981704b7f620c",
+    "url": "/react-videos/static/js/2.e2f50e90.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-videos/static/js/2.e2f50e90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30589aab593373e89433",
+    "url": "/react-videos/static/js/main.fd675f94.chunk.js"
+  },
+  {
+    "revision": "c3576a3a599fdcb86dd4",
+    "url": "/react-videos/static/js/runtime-main.fa6908ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-videos/static/media/logo.5d5d9eef.svg"
+  }
+]);
