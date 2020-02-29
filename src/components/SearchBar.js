2fc5cfe891e44f20d3baf3f6aa7fce415fc65bae
tatjama <tanja120a@gmail.com>
2020-02-29 14:40:47 +0100

    onInputChange and onFormSubmit events created

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+    state = {term:''}
+
+    onInputChange = (event)=>{        
+        this.setState({term: event.target.value})
+        console.log(this.state.term)
+    }
+
+    onFormSubmit = (event) =>{
+        event.preventDefault();
+        console.log(this.state.term)
+    }
 
     render(){
+        
         return(
-            <div>
-                Search Bar
+            <div className = "ui segment">
+                <form onSubmit = {this.onFormSubmit} className = "ui form">
+                    <div className="field">
+                        <label>Search video</label>
+                        <input value={this.state.term} onChange={this.onInputChange} type="text"/>
+                    </div>
+                </form>
             </div>
         );
     }
