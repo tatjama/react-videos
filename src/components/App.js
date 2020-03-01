a9778b2390645a4bc02918a2436aaa27c285e1c1
tatjama <tanja120a@gmail.com>
2020-03-01 18:02:54 +0100

    Cfreaye VideoList.js and display on the screen list of videos

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import youtube from '../api/youtube.js';
 import React from 'react';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList.js';
 
 const KEY = 'AIzaSyBFCzah_fbLs9U0pd9oGeUY4eWr2J4t6CA';
 
 class App extends React.Component{
+    state={videos:[]};
 
     onSearchSubmit =async (term)=>{
-console.log(term)
-   const response =  await youtube.get('/search',{
+    console.log(term)
+    const response =  await youtube.get('/search',{
             params:{
                 q : term,
                 key: KEY,        
@@ -18,12 +20,23 @@ console.log(term)
             }
         })
         console.log(response.data.items);
+        this.setState({videos: response.data.items})
+        console.log(this.state.videos)
     } 
 
     render(){
+       console.log(this.state.videos)
         return(
             <div className = "ui container">
                 <SearchBar onSearchSubmit = {this.onSearchSubmit}/>
+               <div className = "ui grid">
+                   <div className="eleven wide column">
+                        Video display
+                   </div>
+                   <div className = "five wide column">
+                    <VideoList videos = {this.state.videos} />
+                    </div>
+                </div>
                 App
             </div>
         );
---
+++ src/components/VideoList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const VideoList = (props) =>{
+
+   console.log(props.videos);
+  const listVideo = props.videos.map((video)=>{
+       return(           
+               <div className="item">
+                    <img style = {{width: "180px"}} 
+                        className="ui image"
+                        alt={video.snippet.title}
+                        src={video.snippet.thumbnails.medium.url}/>
+                    <div className = "content">
+                        <h4 className="header">{video.snippet.title}</h4>
+                        <p className="description">{video.snippet.description}</p>
+                    </div>
+               </div>        
+       )
+   })    
+        
+        return(
+            <div className="ui list">
+                {listVideo}
+            </div>
+        )
+    
+}
+
+export default VideoList;
