3a0e8e1c6aa716c27850d5c815b5f47492e68893
tatjama <tanja120a@gmail.com>
2020-03-01 07:03:40 +0100

    Signed-off-by: tatjama <tanja120a@gmail.com> install axios, and connect with youtube API

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
+import youtube from '../api/youtube.js';
 import React from 'react';
 import SearchBar from './SearchBar';
 
+const KEY = 'AIzaSyBFCzah_fbLs9U0pd9oGeUY4eWr2J4t6CA';
+
 class App extends React.Component{
+
+    onSearchSubmit =async (term)=>{
+console.log(term)
+   const response =  await youtube.get('/search',{
+            params:{
+                q : term,
+                key: KEY,        
+                part: 'snippet',
+                maxResults: 5,
+                type: "video"
+            }
+        })
+        console.log(response.data.items);
+    } 
 
     render(){
         return(
             <div className = "ui container">
-                <SearchBar/>
+                <SearchBar onSearchSubmit = {this.onSearchSubmit}/>
                 App
             </div>
         );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,6 +11,7 @@ class SearchBar extends React.Component{
     onFormSubmit = (event) =>{
         event.preventDefault();
         console.log(this.state.term)
+        this.props.onSearchSubmit(this.state.term);
     }
 
     render(){
